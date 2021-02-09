81b4fed711d6e55d821fa86c8f5e56c8cf9686be
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-09 17:08:51 -0500

    refactor random btn logic using GifGet
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

--- src/gif-get.js
+++ src/gif-get.js
@@ -1,8 +1,8 @@
 export default class GifGet {
-  static getGif(keyword) {
+  static getGif(keyword, type) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+      const urlSearch = `https://api.giphy.com/v1/gifs/${type}?api_key=${process.env.API_KEY}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -15,23 +15,3 @@ export default class GifGet {
     });
   }
 }
-    
-
-
-// export default class WeatherService {  
-//   static getWeather(city) {
-//     return new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         } else {
-//           reject(request.response);
-//         }
-//       }
-//       request.open("GET", url, true);
-//       request.send();
-//     });
-//   }
-// }
--- src/main.js
+++ src/main.js
@@ -11,10 +11,11 @@ $(document).ready(function(){
     $("#display-search-gif").empty();
     $(".footer").show();
     if (keyword === "") {
-      $("display-search-gif").append('<p>Please enter a keyword</p>');
+      $("#display-search-gif").append('<p>Please enter a keyword</p>');
       return undefined;
     } else {
-      let promise = GifGet.getGif(keyword);
+      const type = 'search';
+      let promise = GifGet.getGif(keyword, type);
       promise.then(function(response) {
         const responseBody = JSON.parse(response);
         const embedLinksArr = getElements(responseBody);
@@ -22,60 +23,34 @@ $(document).ready(function(){
           $("div#display-search-gif").append(`<iframe src="${embedLinksArr[i]}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
         }
       }, function(error) {
-
-      })
-      
-      
-    
-      function getElements(response) {
-        let embedLinksArr = response.data.map(function(elem) {
+        console.log(error);
+      }); 
+    }
+    function getElements(response) {
+      let embedLinksArr = response.data.map(function(elem) {
         return elem.embed_url;
-        });
-        return embedLinksArr;
-      }
-  }
-     }
- 
+      });
+      return embedLinksArr;
+    }
   });
-
-
   $("#random-btn").click(function(){
-    const searchWord = $("#keyword").val();
+    const keyword = $("#keyword").val();
     $("#display-random-gif").empty();
     $("#display-search-gif").empty();
     $(".footer").show();
-
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=${searchWord}&rating=g`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-    
-    request.open("GET", url, true);
-    request.send();
-    
-    function getElements(response) {
-      let link = response.data.embed_url;
-      $("#display-random-gif").append(`<iframe src="${link}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
-    }
-  });
-});
-
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    clearFields();
-    let promise = WeatherService.getWeather(city);
+    const type = 'random';
+    let promise = GifGet.getGif(keyword, type);
     promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      const responseBody = JSON.parse(response);
+      function getElement(responseBody) {
+        let link = responseBody.data.embed_url;
+        return link;
+      }
+      let link = getElement(responseBody);
+      $("#display-random-gif").append(`<iframe src="${link}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
     }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      console.log(error);
     });
   });
+    
 });
